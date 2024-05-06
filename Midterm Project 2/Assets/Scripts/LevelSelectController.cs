1bb0e884e6ddeadf1620fa81d16663385345907f
MaverickSpore <mattmanweaver@gmail.com>
2024-05-06 17:39:01 -0400

    Main Menu Beta
    - Basic Main Menu Made
    - Level select with "Test Levels" made

---
+++ Midterm Project 2/Assets/Scripts/LevelSelectController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class LevelSelectController : MonoBehaviour
+{
+    [SerializeField] List<SelectableLevels> levelsList;
+    [SerializeField] List<GameObject> levelObjects;
+
+    public int levelSelectCount;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        foreach (var level in levelsList)
+        {
+            levelObjects.Add(Instantiate(level.levelModel, new Vector3(-1000,-1000,-1000), new Quaternion(5,5,0,30)));
+        }
+    }
+
+    private void OnEnable()
+    {
+        levelObjects[levelSelectCount].transform.position = new Vector3(0, 0, 0);
+    }
+    public void OnDisable()
+    {
+        foreach (var item in levelObjects)
+        {
+            item.transform.position = new Vector3(-1000, -1000, -1000);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PressNext()
+    {
+        if(levelSelectCount < levelsList.Count - 1)
+        {
+            levelObjects[levelSelectCount].transform.position = new Vector3(-1000, -1000, -1000);
+            levelSelectCount++;
+            levelObjects[levelSelectCount].transform.position = new Vector3(0, 0, 0);
+        }
+        else
+        {
+            levelObjects[levelSelectCount].transform.position = new Vector3(-1000, -1000, -1000);
+            levelSelectCount = 0;
+            levelObjects[levelSelectCount].transform.position = new Vector3(0, 0, 0);
+        }
+    }
+    public void PressPrev()
+    {
+        if (levelSelectCount > 0)
+        {
+            levelObjects[levelSelectCount].transform.position = new Vector3(-1000, -1000, -1000);
+            levelSelectCount--;
+            levelObjects[levelSelectCount].transform.position = new Vector3(0, 0, 0);
+        }
+        else
+        {
+            levelObjects[levelSelectCount].transform.position = new Vector3(-1000, -1000, -1000);
+            levelSelectCount = levelsList.Count - 1;
+            levelObjects[levelSelectCount].transform.position = new Vector3(0, 0, 0);
+        }
+    }
+}
