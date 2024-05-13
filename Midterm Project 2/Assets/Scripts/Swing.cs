dcd6cdd8a4f5f85c76a8551a0302dbd8d8b2040b
MrItzDoubled <90977750+MrItzDoubled@users.noreply.github.com>
2024-05-13 15:55:38 -0400

    Just quickly tweaked Swing.cs
    Quick tweak to make it swing from a sine function.
    I left the old code commented in case anyone needs it.

--- Midterm Project 2/Assets/Scripts/Swing.cs
+++ Midterm Project 2/Assets/Scripts/Swing.cs
@@ -5,66 +5,84 @@ using UnityEngine;
 
 public class Swing : MonoBehaviour
 {
-    float lerpedValue;
-    [SerializeField] float currentRotation;
-    [SerializeField] bool isSwingingForward;
-    [SerializeField] float elapsedTime;
-    [SerializeField] float from;
-    [SerializeField] float to;
-    Quaternion start;
-    Quaternion target;
-    [SerializeField] float targetRotationHeight;
-    [SerializeField] float roatationSpeed;
-    [SerializeField] float rotationDuration;
+    [SerializeField] float amplitude;
+    [SerializeField] float frequency;
+    [SerializeField] float offset;
+    Quaternion startRot;
+    Vector3 axis;
 
-    void Start()
+    private void Start()
     {
-        isSwingingForward = true;
-        elapsedTime = 0;
-        //start = Quaternion.Euler(new Vector3(0, 0, transform.rotation.z));
-        //target = Quaternion.Euler(new Vector3(0, 0, targetRotationHeight));
-        from = transform.rotation.z;
-        to = targetRotationHeight;
+        startRot = transform.rotation;
+        axis = transform.forward;
     }
 
-    void Update()
+    private void Update()
     {
-        //transform.rotation = Quaternion.Lerp(from, to, elapsedTime * roatationSpeed);
-
-        currentRotation = transform.rotation.z;
-        if (true == isSwingingForward)
-        {
-            //lerp rotation to targetRotationHeight;
-            float t = elapsedTime / rotationDuration;
-            lerpedValue = Mathf.Lerp(from, to, t);
-            transform.rotation = Quaternion.Euler(0, 0, lerpedValue);
-            elapsedTime += Time.deltaTime;
-            if (elapsedTime >= rotationDuration)
-            {
-                //from = transform.rotation.z;
-                from = targetRotationHeight;
-                to = -targetRotationHeight;
-                elapsedTime = 0;
-                isSwingingForward = false;
-            }
-        }
-        else if (false == isSwingingForward)
-        {
-            //lerp rotation to -targetRotationHeight;
-            float t = elapsedTime / rotationDuration;
-            lerpedValue = Mathf.Lerp(from, to, t);
-            transform.rotation = Quaternion.Euler(0, 0, lerpedValue);
-            elapsedTime += Time.deltaTime;
-            if (elapsedTime >= rotationDuration)
-            {
-                //from = transform.rotation.z;
-                from = -targetRotationHeight;
-                to = targetRotationHeight;
-                elapsedTime = 0;
-                isSwingingForward = true;
-            }
-        }
-        //elapsedTime += Time.deltaTime;
-
+        transform.rotation = Quaternion.AngleAxis(amplitude * Mathf.Cos(Time.time * frequency + offset), axis) * startRot;
     }
+
+
+    //float lerpedValue;
+    //[SerializeField] float currentRotation;
+    //[SerializeField] bool isSwingingForward;
+    //[SerializeField] float elapsedTime;
+    //[SerializeField] float from;
+    //[SerializeField] float to;
+    //Quaternion start;
+    //Quaternion target;
+    //[SerializeField] float targetRotationHeight;
+    //[SerializeField] float roatationSpeed;
+    //[SerializeField] float rotationDuration;
+    //
+    //void Start()
+    //{
+    //    isSwingingForward = true;
+    //    elapsedTime = 0;
+    //    //start = Quaternion.Euler(new Vector3(0, 0, transform.rotation.z));
+    //    //target = Quaternion.Euler(new Vector3(0, 0, targetRotationHeight));
+    //    from = transform.rotation.z;
+    //    to = targetRotationHeight;
+    //}
+    //
+    //void Update()
+    //{
+    //    //transform.rotation = Quaternion.Lerp(from, to, elapsedTime * roatationSpeed);
+    //
+    //    currentRotation = transform.rotation.z;
+    //    if (true == isSwingingForward)
+    //    {
+    //        //lerp rotation to targetRotationHeight;
+    //        float t = elapsedTime / rotationDuration;
+    //        lerpedValue = Mathf.Lerp(from, to, t);
+    //        transform.rotation = Quaternion.Euler(0, 0, lerpedValue);
+    //        elapsedTime += Time.deltaTime;
+    //        if (elapsedTime >= rotationDuration)
+    //        {
+    //            //from = transform.rotation.z;
+    //            from = targetRotationHeight;
+    //            to = -targetRotationHeight;
+    //            elapsedTime = 0;
+    //            isSwingingForward = false;
+    //        }
+    //    }
+    //    else if (false == isSwingingForward)
+    //    {
+    //        //lerp rotation to -targetRotationHeight;
+    //        float t = elapsedTime / rotationDuration;
+    //        lerpedValue = Mathf.Lerp(from, to, t);
+    //        transform.rotation = Quaternion.Euler(0, 0, lerpedValue);
+    //        elapsedTime += Time.deltaTime;
+    //        if (elapsedTime >= rotationDuration)
+    //        {
+    //            //from = transform.rotation.z;
+    //            from = -targetRotationHeight;
+    //            to = targetRotationHeight;
+    //            elapsedTime = 0;
+    //            isSwingingForward = true;
+    //        }
+    //    }
+    //    //elapsedTime += Time.deltaTime;
+    //
+    //}
 }
