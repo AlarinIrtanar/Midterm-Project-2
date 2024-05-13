26f0b5edbd8d7539b1fd90440886c75f8b0dddc1
MaverickSpore <mattmanweaver@gmail.com>
2024-05-13 09:54:04 -0400

    Options Menu Added
    - Added options menu button to the main menu
    - Options menu is still empty for now

--- Midterm Project 2/Assets/Scripts/Managers/FileManager.cs
+++ Midterm Project 2/Assets/Scripts/Managers/FileManager.cs
@@ -9,6 +9,7 @@ public class FileManager : MonoBehaviour
 {
     public static FileManager instance;
     string filePath;
+    string optionsPath;
     public List<bool> levelUnlocks;
 
     // Start is called before the first frame update
@@ -16,6 +17,7 @@ public class FileManager : MonoBehaviour
     {
         instance = this;
         filePath = Application.persistentDataPath;
+        optionsPath = filePath + "/options.dat";
     }
 
 
@@ -44,6 +46,27 @@ public class FileManager : MonoBehaviour
 
             levelUnlocks = (List<bool>)bin.Deserialize(fin);
             fin.Close();
+        }
+    }
+    public void SaveOptions()
+    {
+        BinaryFormatter bout = new BinaryFormatter();
+        FileStream fout = File.Open(optionsPath, FileMode.OpenOrCreate);
+
+        fout.Close();
+    }
+    public void LoadOptions()
+    {
+        if (File.Exists(optionsPath))
+        {
+            BinaryFormatter bin = new BinaryFormatter();
+            FileStream fin = File.Open(optionsPath, FileMode.Open);
+
+            fin.Close();
+        }
+        else
+        {
+            SaveOptions();
         }
     }
 }
