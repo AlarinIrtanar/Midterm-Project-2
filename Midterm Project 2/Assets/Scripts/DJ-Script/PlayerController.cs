397a6fb414c14b48880942b0636c1164f6e3176a
huggins9000211 <39134424+huggins9000211@users.noreply.github.com>
2024-05-14 13:36:35 -0400

    Player movement audio

--- Midterm Project 2/Assets/Scripts/DJ-Script/PlayerController.cs
+++ Midterm Project 2/Assets/Scripts/DJ-Script/PlayerController.cs
@@ -27,7 +27,7 @@ public class PlayerController : MonoBehaviour, IDamage
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Shoot") && !isShooting)
+        if (Input.GetButton("Shoot") && !isShooting && !MenuManager.instance.isPaused)
         {
             StartCoroutine(shoot());
         }
