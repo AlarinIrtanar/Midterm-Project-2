4598bbb12bade3fc9d107c19bd8d3f47d194567e
huggins9000211 <39134424+huggins9000211@users.noreply.github.com>
2024-05-14 11:30:52 -0400

    start of playerAdio

--- Midterm Project 2/Assets/Scripts/PlayerAudio.cs
+++ Midterm Project 2/Assets/Scripts/PlayerAudio.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerAudio : MonoBehaviour
 {
+
+    [SerializeField] PlayerMovement playerMovement;
+    [SerializeField] AudioSource FootstepSource;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,14 @@ public class PlayerAudio : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //if ( playerMovement.state == PlayerMovement.MovementState.Walking || playerMovement.state == PlayerMovement.MovementState.Sprinting)
+        //{
+        //    Debug
+        //}
     }
+
+    //IEnumerator PlayFootseps()
+    //{
+    //    //yield return waitfor
+    //}
 }
