02d88f0da0339a4e094f3ccde8d9c7c6c5a5498e
MrItzDoubled <90977750+MrItzDoubled@users.noreply.github.com>
2024-05-29 12:21:42 -0400

    Fixed jump pad inconsistency

--- Midterm Project 2/Assets/Scripts/JumpPad.cs
+++ Midterm Project 2/Assets/Scripts/JumpPad.cs
@@ -6,14 +6,28 @@ public class JumpPad : MonoBehaviour
 {
     [SerializeField] float launchSpeed;
     [SerializeField] AudioSource jumpSource;
+    Rigidbody rb;
+    float timer;
+
+    private void Update()
+    {
+        if (timer > 0f && rb != null)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, launchSpeed * 0.9f, rb.velocity.z);
+            timer -= Time.deltaTime;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             jumpSource.Play();
-            other.attachedRigidbody.AddForce(transform.up * launchSpeed, ForceMode.Impulse);
-            
+            rb = other.attachedRigidbody;
+            rb.velocity = new Vector3(rb.velocity.x, launchSpeed * 0.9f, rb.velocity.z);
+            timer = 0.05f;
+            //other.attachedRigidbody.AddForce(transform.up * launchSpeed, ForceMode.Impulse);
         }
     }
+
 }
